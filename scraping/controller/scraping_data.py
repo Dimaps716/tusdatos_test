22b2a93294f17c379834cf4d18bef007839a1bcc
Dimanso <dimaps716@gmail.com>
2024-05-09 16:13:10 -0500

    scraping_data

---
+++ scraping/controller/scraping_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from fastapi import APIRouter, HTTPException, status, BackgroundTasks
+
+from scraping.get_detalles_de_procesos import obtener_datos_incidente_judicatura
+from scraping.model.scraping_model import ScrapingDataRequest
+from scraping.scraping_procesos_judiciales import scraping_procesos
+from settings import Settings
+
+settings = Settings()
+
+router = APIRouter(prefix="/Scraping")
+
+
+@router.post(
+    "/scraping",
+    tags=["Scraping"],
+    status_code=status.HTTP_200_OK,
+    summary="Scraping de datos",
+    description="Obtiene datos de scraping según los ID de actores y demandados proporcionados.",
+)
+def scraping_data(data: ScrapingDataRequest, background_tasks: BackgroundTasks):
+    """
+    Obtiene datos de scraping según los ID de actores y demandados proporcionados.
+    """
+    if not data.actor_id and not data.demandado_id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Se requiere al menos un ID de actor o demandado.",
+        )
+
+    scraping_procesos_data = scraping_procesos(data.actor_id, data.demandado_id)
+
+    df = pd.DataFrame(scraping_procesos_data)
+    data_id = df["idJuicio"].tolist()
+
+    background_tasks.add_task(obtener_datos_incidente_judicatura, data_id)
+
+    return scraping_procesos_data
