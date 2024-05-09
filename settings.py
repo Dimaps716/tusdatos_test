0c508874e7722e6c6115c234ddcb6d4f49274cee
Dimanso <dimaps716@gmail.com>
2024-05-09 16:12:28 -0500

    settings.py

---
+++ settings.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+
+    ROOT_PATH = ""
+
+    ALLOW_ORIGINS = os.getenv("ALLOW_ORIGINS")
+    ALLOW_CREDENTIALS = os.getenv("ALLOW_CREDENTIALS")
+    ALLOW_METHODS = os.getenv("ALLOW_METHODS")
+    ALLOW_HEADERS = os.getenv("ALLOW_HEADERS")
+    
+    BASE_URL_API = "https://api.funcionjudicial.gob.ec/EXPEL-CONSULTA-CAUSAS-SERVICE/api/consulta-causas/informacion/"
